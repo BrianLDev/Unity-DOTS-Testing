011f3133bf59c7c285c54fd5eed14453fe3be171
Brian Leip <brianleip@gmail.com>
2022-04-28 09:24:05 -0700

    pickup entity removal now works...sometimes. Unclear why it works in some cases and not others. May be Unity bug.

--- DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/PickupOnTriggerSystem.cs
+++ DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/PickupOnTriggerSystem.cs
@@ -6,30 +6,34 @@ using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
 
-[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
-[UpdateAfter(typeof(EndFramePhysicsSystem))]
+// [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+// [UpdateAfter(typeof(EndFramePhysicsSystem))]
 public partial class PickupOnTriggerSystem : SystemBase
 {
   // private BuildPhysicsWorld buildPhysicsWorld;  // setting up colliders and entities
   private StepPhysicsWorld stepPhysicsWorld;    // running simulation
-  private EndFixedStepSimulationEntityCommandBufferSystem commandBufferSystem;
+  private EndSimulationEntityCommandBufferSystem commandBufferSystem;
 
   protected override void OnCreate()
   {
     base.OnCreate();
     // buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
     stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
-    commandBufferSystem = World.GetOrCreateSystem<EndFixedStepSimulationEntityCommandBufferSystem>();
+    commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
   }
 
   protected override void OnUpdate()
   {
-    Dependency = new PickupOnTriggerSystemJob
+    PickupOnTriggerSystemJob pickupOnTriggerSystemJob = new PickupOnTriggerSystemJob
     {
       allPickups = GetComponentDataFromEntity<PickupTag>(true),
       allPlayers = GetComponentDataFromEntity<PlayerTag>(true),
       entityCommandBuffer = commandBufferSystem.CreateCommandBuffer()
-    }.Schedule(stepPhysicsWorld.Simulation, Dependency);
+    };
+
+    Dependency = pickupOnTriggerSystemJob.Schedule(stepPhysicsWorld.Simulation, Dependency);
+
+    commandBufferSystem.AddJobHandleForProducer(Dependency);
     Dependency.Complete();
   }
 
@@ -46,9 +50,13 @@ public partial class PickupOnTriggerSystem : SystemBase
       Entity entityB = triggerEvent.EntityB;
 
       if (allPlayers.HasComponent(entityA) && allPickups.HasComponent(entityB))
+      {
         entityCommandBuffer.DestroyEntity(entityB);
+      }
       else if (allPlayers.HasComponent(entityB) && allPickups.HasComponent(entityA))
+      {
         entityCommandBuffer.DestroyEntity(entityA);
+      }
 
     }
   }
