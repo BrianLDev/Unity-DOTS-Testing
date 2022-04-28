d0c01633256256532c17d6989cc9e3c9cbdab9ab
Brian Leip <brianleip@gmail.com>
2022-04-28 12:18:38 -0700

    Fixed pickup bug. Added a lot of pickups for testing and fun

--- DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/PickupOnTriggerSystem.cs
+++ DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/PickupOnTriggerSystem.cs
@@ -6,20 +6,21 @@ using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
 
-// [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
-// [UpdateAfter(typeof(EndFramePhysicsSystem))]
 public partial class PickupOnTriggerSystem : SystemBase
 {
-  // private BuildPhysicsWorld buildPhysicsWorld;  // setting up colliders and entities
   private StepPhysicsWorld stepPhysicsWorld;    // running simulation
   private EndSimulationEntityCommandBufferSystem commandBufferSystem;
 
   protected override void OnCreate()
   {
     base.OnCreate();
-    // buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
     stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
     commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+  }
+
+  protected override void OnStartRunning()
+  {
+    this.RegisterPhysicsRuntimeSystemReadOnly();
   }
 
   protected override void OnUpdate()
@@ -27,27 +28,28 @@ public partial class PickupOnTriggerSystem : SystemBase
     PickupOnTriggerSystemJob pickupOnTriggerSystemJob = new PickupOnTriggerSystemJob
     {
       allPickups = GetComponentDataFromEntity<PickupTag>(true),
-      allPlayers = GetComponentDataFromEntity<PlayerTag>(true),
+      allPlayers = GetComponentDataFromEntity<PlayerTag>(),
       entityCommandBuffer = commandBufferSystem.CreateCommandBuffer()
     };
 
     Dependency = pickupOnTriggerSystemJob.Schedule(stepPhysicsWorld.Simulation, Dependency);
-
-    commandBufferSystem.AddJobHandleForProducer(Dependency);
     Dependency.Complete();
+    commandBufferSystem.AddJobHandleForProducer(Dependency);
   }
 
   [BurstCompile]
   public struct PickupOnTriggerSystemJob : ITriggerEventsJob
   {
     [ReadOnly] public ComponentDataFromEntity<PickupTag> allPickups;
-    [ReadOnly] public ComponentDataFromEntity<PlayerTag> allPlayers;
+    public ComponentDataFromEntity<PlayerTag> allPlayers;
     public EntityCommandBuffer entityCommandBuffer;
 
     public void Execute(TriggerEvent triggerEvent)
     {
       Entity entityA = triggerEvent.EntityA;
       Entity entityB = triggerEvent.EntityB;
+
+      if (allPickups.HasComponent(entityA) && allPickups.HasComponent(entityB)) return;
 
       if (allPlayers.HasComponent(entityA) && allPickups.HasComponent(entityB))
       {
