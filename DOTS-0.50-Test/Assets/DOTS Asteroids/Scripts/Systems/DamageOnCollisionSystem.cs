acd290eb9430d2ce24a74edaf7461a39c980a171
Brian Leip <brianleip@gmail.com>
2022-04-29 20:32:56 -0700

    Damage on collision and destroy now working at super fast FPS

--- DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/DamageOnCollisionSystem.cs
+++ DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/DamageOnCollisionSystem.cs
@@ -5,52 +5,52 @@ using Unity.Collections;
 using Unity.Physics;
 using Unity.Physics.Systems;
 
-// public partial class DamageOnCollisionSystem : SystemBase
-// {
-//   private BuildPhysicsWorld buildPhysicsWorld;
-//   private StepPhysicsWorld stepPhysicsWorld;
+public partial class DamageOnCollisionSystem : SystemBase
+{
+  private BuildPhysicsWorld buildPhysicsWorld;
+  private StepPhysicsWorld stepPhysicsWorld;
 
-//   protected override void OnCreate()
-//   {
-//     base.OnCreate();
-//     buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
-//     stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
-//   }
+  protected override void OnCreate()
+  {
+    base.OnCreate();
+    buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+    stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+  }
 
-//   protected override void OnUpdate()
-//   {
-//     Dependency = new DamageOnCollisionSystemJob
-//     {
-//       healthGroup = GetComponentDataFromEntity<HealthData>()
-//     }.Schedule(stepPhysicsWorld.Simulation, Dependency);
+  protected override void OnUpdate()
+  {
+    Dependency = new DamageOnCollisionSystemJob
+    {
+      healthGroup = GetComponentDataFromEntity<HealthData>()
+    }.Schedule(stepPhysicsWorld.Simulation, Dependency);
     
-//     Dependency.Complete();
-//   }
+    Dependency.Complete();
+  }
 
-//   [BurstCompile]
-//   public struct DamageOnCollisionSystemJob : ICollisionEventsJob
-//   {
-//     public ComponentDataFromEntity<HealthData> healthGroup;
+  [BurstCompile]
+  public struct DamageOnCollisionSystemJob : ICollisionEventsJob
+  {
+    public ComponentDataFromEntity<HealthData> healthGroup;
 
-//     public void Execute(CollisionEvent collisionEvent)
-//     {
-//       Entity entityA = collisionEvent.EntityA;
-//       Entity entityB = collisionEvent.EntityB;
+    public void Execute(CollisionEvent collisionEvent)
+    {
+      Entity entityA = collisionEvent.EntityA;
+      Entity entityB = collisionEvent.EntityB;
 
-//       if (healthGroup.HasComponent(entityA) && healthGroup.HasComponent(entityB)) 
-//       {
-//         // make a copy of data, modify it, then copy back
-//         HealthData healthA = healthGroup[entityA];
-//         HealthData healthB = healthGroup[entityB];
-//         healthA.health -= healthB.damageCaused;
-//         healthB.health -= healthA.damageCaused;
-//         if (healthA.health <= 0)
-//           healthA.isDead = true;
-//         if (healthB.health <= 0)
-//           healthB.isDead = true;
-//         healthGroup[entityA] = healthA;
-//         healthGroup[entityB] = healthB;
-//       }
-//     }
-//   }
-// }
+      if (healthGroup.HasComponent(entityA) && healthGroup.HasComponent(entityB)) 
+      {
+        // make a copy of data, modify it, then copy back
+        HealthData healthA = healthGroup[entityA];
+        HealthData healthB = healthGroup[entityB];
+        healthA.health -= healthB.damageCaused;
+        healthB.health -= healthA.damageCaused;
+        if (healthA.health <= 0)
+          healthA.isDead = true;
+        if (healthB.health <= 0)
+          healthB.isDead = true;
+        healthGroup[entityA] = healthA;
+        healthGroup[entityB] = healthB;
+      }
+    }
+  }
+}
--- DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/DestroyOnDeathSystem.cs
+++ DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/DestroyOnDeathSystem.cs
@@ -2,33 +2,33 @@ using Unity.Entities;
 using Unity.Transforms;
 using Unity.Physics.Systems;
 
-// [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
-// [UpdateBefore(typeof(BuildPhysicsWorld))]
-// public partial class DestroyOnDeathSystem : SystemBase
-// {
-  // private EndSimulationEntityCommandBufferSystem commandBufferSystem;
+[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+[UpdateBefore(typeof(BuildPhysicsWorld))]
+public partial class DestroyOnDeathSystem : SystemBase
+{
+  private EndSimulationEntityCommandBufferSystem commandBufferSystem;
 
-  // protected override void OnCreate()
-  // {
-  //   base.OnCreate();
-  //   commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-  // }
+  protected override void OnCreate()
+  {
+    base.OnCreate();
+    commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+  }
 
-  // protected override void OnUpdate()
-  // {
-  //   EntityCommandBuffer ecb = commandBufferSystem.CreateCommandBuffer();
+  protected override void OnUpdate()
+  {
+    EntityCommandBuffer ecb = commandBufferSystem.CreateCommandBuffer();
 
-  //   Entities.
-  //     WithAny<ChaserTag, AsteroidTag>().
-  //     ForEach((Entity entity, in HealthData healthData) =>
-  //   {
-  //     if (healthData.isDead && entity != Entity.Null)
-  //     {
-  //       ecb.DestroyEntity(entity);
-  //     }
-  //   }).Run();
+    Entities.
+      WithAny<ChaserTag, AsteroidTag>().
+      ForEach((Entity entity, in HealthData healthData) =>
+    {
+      if (healthData.isDead && entity != Entity.Null)
+      {
+        ecb.DestroyEntity(entity);
+      }
+    }).Run();
 
-  //   this.Dependency.Complete();
-  //   commandBufferSystem.AddJobHandleForProducer(this.Dependency);
-  // }
-// }
+    this.Dependency.Complete();
+    commandBufferSystem.AddJobHandleForProducer(this.Dependency);
+  }
+}
