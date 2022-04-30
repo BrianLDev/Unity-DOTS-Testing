ba337acad238b3d68973489c2ebcfdf8b4ce991e
Brian Leip <brianleip@gmail.com>
2022-04-29 20:48:42 -0700

    Pickup on trigger working with new physics mvmt system. And fast

--- DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/PickupOnTriggerSystem.cs
+++ DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/PickupOnTriggerSystem.cs
@@ -6,61 +6,61 @@ using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
 
-// public partial class PickupOnTriggerSystem : SystemBase
-// {
-//   private StepPhysicsWorld stepPhysicsWorld;    // running simulation
-//   private EndSimulationEntityCommandBufferSystem commandBufferSystem;
+public partial class PickupOnTriggerSystem : SystemBase
+{
+  private StepPhysicsWorld stepPhysicsWorld;    // running simulation
+  private EndSimulationEntityCommandBufferSystem commandBufferSystem;
 
-//   protected override void OnCreate()
-//   {
-//     base.OnCreate();
-//     stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
-//     commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-//   }
+  protected override void OnCreate()
+  {
+    base.OnCreate();
+    stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+    commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+  }
 
-//   protected override void OnStartRunning()
-//   {
-//     this.RegisterPhysicsRuntimeSystemReadOnly();
-//   }
+  protected override void OnStartRunning()
+  {
+    this.RegisterPhysicsRuntimeSystemReadOnly();
+  }
 
-//   protected override void OnUpdate()
-//   {
-//     var entityCommandBuffer = commandBufferSystem.CreateCommandBuffer();
-//     Dependency = new PickupOnTriggerSystemJob
-//     {
-//       allPickups = GetComponentDataFromEntity<PickupTag>(true),
-//       allPlayers = GetComponentDataFromEntity<PlayerTag>(),
-//       ecb = entityCommandBuffer
-//     }.Schedule(stepPhysicsWorld.Simulation, Dependency);
+  protected override void OnUpdate()
+  {
+    var entityCommandBuffer = commandBufferSystem.CreateCommandBuffer();
+    Dependency = new PickupOnTriggerSystemJob
+    {
+      allPickups = GetComponentDataFromEntity<PickupTag>(true),
+      allPlayers = GetComponentDataFromEntity<PlayerTag>(),
+      ecb = entityCommandBuffer
+    }.Schedule(stepPhysicsWorld.Simulation, Dependency);
     
-//     Dependency.Complete();
-//     commandBufferSystem.AddJobHandleForProducer(this.Dependency);
-//   }
+    Dependency.Complete();
+    commandBufferSystem.AddJobHandleForProducer(this.Dependency);
+  }
 
-//   [BurstCompile]
-//   public struct PickupOnTriggerSystemJob : ITriggerEventsJob
-//   {
-//     [ReadOnly] public ComponentDataFromEntity<PickupTag> allPickups;
-//     public ComponentDataFromEntity<PlayerTag> allPlayers;
-//     public EntityCommandBuffer ecb;
+  [BurstCompile]
+  public struct PickupOnTriggerSystemJob : ITriggerEventsJob
+  {
+    [ReadOnly] public ComponentDataFromEntity<PickupTag> allPickups;
+    public ComponentDataFromEntity<PlayerTag> allPlayers;
+    public EntityCommandBuffer ecb;
 
-//     public void Execute(TriggerEvent triggerEvent)
-//     {
-//       Entity entityA = triggerEvent.EntityA;
-//       Entity entityB = triggerEvent.EntityB;
+    public void Execute(TriggerEvent triggerEvent)
+    {
+      Entity entityA = triggerEvent.EntityA;
+      Entity entityB = triggerEvent.EntityB;
 
-//       if (!allPickups.HasComponent(entityA) && !allPickups.HasComponent(entityB)) return;
-//       if (allPickups.HasComponent(entityA) && allPickups.HasComponent(entityB)) return;
+      if (!allPickups.HasComponent(entityA) && !allPickups.HasComponent(entityB)) return;
+      if (allPickups.HasComponent(entityA) && allPickups.HasComponent(entityB)) return;
 
-//       if (allPlayers.HasComponent(entityA) && allPickups.HasComponent(entityB))
-//       {
-//         ecb.DestroyEntity(entityB);
-//       }
-//       else if (allPlayers.HasComponent(entityB) && allPickups.HasComponent(entityA))
-//       {
-//         ecb.DestroyEntity(entityA);
-//       }
+      if (allPlayers.HasComponent(entityA) && allPickups.HasComponent(entityB))
+      {
+        ecb.DestroyEntity(entityB);
+      }
+      else if (allPlayers.HasComponent(entityB) && allPickups.HasComponent(entityA))
+      {
+        ecb.DestroyEntity(entityA);
+      }
 
-//     }
-//   }
-// }
+    }
+  }
+}
