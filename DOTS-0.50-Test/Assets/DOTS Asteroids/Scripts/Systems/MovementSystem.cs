fc7242fc7de46088f97201eca35a4ba36a061ddb
Brian Leip <brianleip@gmail.com>
2022-04-29 20:28:43 -0700

    Improved player movement to zero G thruster style

--- DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/MovementSystem.cs
+++ DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/MovementSystem.cs
@@ -2,20 +2,22 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Physics;
 
-// public partial class MovementSystem : SystemBase
-// {
-// protected override void OnUpdate()
-//   {
-//     float deltaTime = Time.DeltaTime;
+public partial class MovementSystem : SystemBase
+{
+protected override void OnUpdate()
+  {
+    float deltaTime = Time.DeltaTime;
 
-//     Entities.
-//       WithAny<PlayerTag, ChaserTag>().
-//       ForEach((ref Translation pos, in MoveData moveData, in Rotation rot) => 
-//       {
-//         float3 forwardDirection = math.forward(rot.Value);
-//         pos.Value += forwardDirection * moveData.speed * deltaTime;
-//       }).ScheduleParallel();
+    Entities.
+      WithAny<PlayerTag, ChaserTag>().
+      ForEach((ref PhysicsVelocity physicsVelociy, in MoveData moveData, in Rotation rot) => 
+      {
+        float3 forwardDirection = math.forward(rot.Value);
+        if (!moveData.moveDirection.Equals(float3.zero))
+          physicsVelociy.Linear += forwardDirection * moveData.speed * deltaTime;
+      }).ScheduleParallel();
 
-//   }
-// }
+  }
+}
