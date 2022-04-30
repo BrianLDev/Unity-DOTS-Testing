eb5437ce0a977e4d72f912335eac59aa774a063f
Brian Leip <brianleip@gmail.com>
2022-04-29 20:12:49 -0700

    improved asteroid spawner using physics mass

--- DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/AsteroidSpawnerSystem.cs
+++ DOTS-0.50-Test/Assets/DOTS Asteroids/Scripts/Systems/AsteroidSpawnerSystem.cs
@@ -15,15 +15,17 @@ public partial class AsteroidSpawnerSystem : SystemBase
 
     Entities.
       WithAll<AsteroidTag>().
-      ForEach((ref Translation pos, ref CompositeScale scale, ref PhysicsVelocity physicsVelocity) =>
+      ForEach((ref Translation pos, ref CompositeScale scale, ref PhysicsVelocity physicsVelocity, ref PhysicsMass physicsMass) =>
       {
         pos.Value = (rand.NextFloat3()-0.5f) * screenDimensions;
+        pos.Value.z = 0;
         scale.Value = (rand.NextFloat()+0.5f) * 2f * float4x4.identity;
         scale.Value.c3.w = 1;
         physicsVelocity.Linear = rand.NextFloat3Direction()-0.5f;
         physicsVelocity.Linear.z = 0;
         physicsVelocity.Angular = rand.NextFloat3Direction()-0.5f;
-      }).Schedule();
+        physicsMass.InverseMass = 1/(scale.Value.c0.x * 50);
+      }).ScheduleParallel();
 
     this.Dependency.Complete();
   }
